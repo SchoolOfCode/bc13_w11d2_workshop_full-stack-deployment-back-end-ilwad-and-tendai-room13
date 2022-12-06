0d5f34f1d4541d1730c667fc431c823053d7212f
Tendai Karuwo <tendaikaruwo96@gmail.com>
2022-12-06 11:55:09 +0000

    inserted details for create table

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import logger from "morgan";
+const PORT = process.env.PORT;
 import cors from "cors";
 
 import shoppingListRouter from "./routes/shoppingList.js";
@@ -12,5 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/items", shoppingListRouter);
+
+app.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}`);
+});
 
 export default app;
--- db/scripts/createTable.js
+++ db/scripts/createTable.js
@@ -2,11 +2,15 @@ import { pool } from "../index.js";
 
 async function createTable() {
   const created = await pool.query(
-    `CREATE TABLE IF NOT EXISTS shopping (
-      id SERIAL PRIMARY KEY,
+    `CREATE TABLE shopping (
+      id  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
       item TEXT NOT NULL,
       completed BOOL NOT NULL
-    );`
+    );
+ INSERT INTO shopping (item, completed)
+    VALUES 
+    ('Chicken', true), 
+    ('Beans', false);`
   );
   console.log("shopping table created", created.command);
 }
